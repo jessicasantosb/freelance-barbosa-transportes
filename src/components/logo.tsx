82471b030dcc1080d15f1eba64a34f2d11778728
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-05 16:04:54 -0300

    update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Logo() {
   return (
     <div className="flex items-center gap-2 opacity-50">
-      <Image alt="logo" src={"/images/logo.svg"} width={40} height={25} />
+      <Image alt="logo" src={"/images/logo.png"} width={40} height={25} />
       <h4 className="tracking-wider">SIDINEI BARBOSA</h4>
     </div>
   );
