0011495e0b6b72e065fe01c40e4fd690b8f96ec6
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-05 10:14:56 -0300

    change logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export function Logo() {
   return (
-    <p className="tracking-tight text-md">
-      <span className="font-bold">Sidinei</span> Barbosa
-    </p>
+    <div className="flex items-center gap-2">
+      <Image alt="logo" src={"/images/logo.png"} width={35} height={20} />
+      <h4 className="tracking-wider">SIDINEI BARBOSA</h4>
+    </div>
   );
 }
