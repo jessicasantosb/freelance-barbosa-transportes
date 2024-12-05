f93ff21e94d1a3f959a675e408ad5e5f06c5cf01
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-05 20:11:44 -0300

    add footer copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import { Separator } from "./ui/separator";
 
 export function Footer() {
@@ -7,16 +7,21 @@ export function Footer() {
     <>
       <Separator />
 
-      <footer className="pt-8 flex flex-col items-center justify-center gap-4">
-        <Link
-          href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
-          target="_blank"
-          className="p-2 rounded-sm flex items-center gap-1 hover:underline underline-offset-2 select-none hover:bg-gradient-to-r from-[#128c7e]/60 to-[#25d366]/60"
-        >
-          <FaWhatsapp />
-          Whatsapp
-        </Link>
-        <p className="opacity-50">Sidinei Barbosa - Entregas e Fretes</p>
+      <footer className="pt-8 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <p>Sidinei Barbosa - Entregas e Fretes</p>
+          <Link
+            href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
+            target="_blank"
+            className="p-2 rounded-sm flex items-center gap-1 hover:underline underline-offset-2 select-none hover:bg-gradient-to-r from-[#128c7e]/60 to-[#25d366]/60"
+          >
+            <FaWhatsapp />
+            Whatsapp
+          </Link>
+        </div>
+        <p className="pt-4 text-muted-foreground">
+          &copy; Criado por Jessica Bandeira
+        </p>
       </footer>
     </>
   );
