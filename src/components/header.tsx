490e8f9613e10b891735a7e36cdeff5f3c2049fd
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 14:03:03 -0300

    feat: create desktop header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,6 @@
 import { Logo } from "@/components/logo";
+import { Lightbulb, ShieldQuestion } from "lucide-react";
+import Link from "next/link";
 import { HeaderMenu } from "./header-menu";
 import { ModeToggle } from "./mode-toggle";
 
@@ -6,8 +8,27 @@ export function Header() {
   return (
     <header className="w-full h-16 p-4 flex flex-col sm:flex-row items-center justify-between gap-4">
       <Logo />
-      <div className="flex items-center gap-4">
-        <HeaderMenu />
+      <div className="flex items-center gap-6">
+        <div className="hidden md:flex items-center gap-6">
+          <Link
+            href={"/faq"}
+            className="flex items-center gap-1 hover:underline hover:opacity-90"
+          >
+            <ShieldQuestion />
+            Perguntas Frequentes
+          </Link>
+          <Link
+            href={"/dicas-uteis"}
+            className="flex items-center gap-1 hover:underline hover:opacity-90"
+          >
+            <Lightbulb />
+            Dicas úteis
+          </Link>
+        </div>
+
+        <div className="md:hidden">
+          <HeaderMenu />
+        </div>
         <ModeToggle />
       </div>
     </header>
