447af1bbecaef1c60d40cd87a6e4b163d7abafbf
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 09:14:58 -0300

    feat: create logo link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2 opacity-50">
+    <Link href={"/"} className="flex items-center gap-2 opacity-50">
       <Image alt="logo" src={"/images/logo.png"} width={40} height={25} />
       <h4 className="tracking-wider">BARBOSA FRETES</h4>
-    </div>
+    </Link>
   );
 }
