86f46c9d359bfd1b720bda99b7a62c2079e96002
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 09:01:04 -0300

    feat: update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="flex items-center gap-2 opacity-50">
       <Image alt="logo" src={"/images/logo.png"} width={40} height={25} />
-      <h4 className="tracking-wider">SIDINEI BARBOSA</h4>
+      <h4 className="tracking-wider">BARBOSA FRETES</h4>
     </div>
   );
 }
