b032753c8ecfb0abcbea551f9160dd80b375190f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-09 12:20:36 -0300

    refactor: put header and footer on layout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { Footer, Header, HomeImage, HomeMessage } from "@/components";
+import { HomeImage, HomeMessage } from "@/components";
 
 export default async function Home() {
   return (
-    <main className="container px-2 py-10 md:p-10">
-      <Header />
-      <div className="my-14 flex items-center gap-6">
-        <HomeMessage />
-        <HomeImage />
-      </div>
-      <Footer />
+    <main className="my-14 p-4 flex items-center gap-6">
+      <HomeMessage />
+      <HomeImage />
     </main>
   );
 }
