cb388205267cc0771f1d37bc8ebc61da1b7cff64
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-10 18:52:24 -0300

    feat: create imageCard component

---
+++ src/components/image-card.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export function ImageCard({ imageName }: { imageName: string }) {
+  return (
+    <div className="relative hidden md:flex flex-col-reverse text-right min-w-96 h-96">
+      <Image
+        alt="icon"
+        src={`/images/${imageName}`}
+        fill
+        className="absolute object-cover"
+      />
+      <a
+        href="https://storyset.com/people"
+        className="text-xs text-muted-foreground"
+      >
+        People illustrations by Storyset
+      </a>
+    </div>
+  );
+}
