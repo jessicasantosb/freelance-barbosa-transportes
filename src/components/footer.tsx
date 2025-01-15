f135303cdc3906d0a2f2e69cbd58092bccddae14
Jessica <jessica.santosb@outlook.com>
2025-01-15 15:35:04 -0300

    feat: add instagram link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { Separator } from "./ui/separator";
 
 export function Footer() {
@@ -8,16 +8,26 @@ export function Footer() {
       <Separator />
 
       <footer className="py-4 px-2 flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-2">
           <p>Sidinei Barbosa - Entregas e Fretes em geral</p>
-          <Link
-            href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
-            target="_blank"
-            className="p-2 rounded-sm flex items-center gap-1 hover:underline underline-offset-2 select-none hover:bg-gradient-to-r from-[#128c7e]/60 to-[#25d366]/60"
-          >
-            <FaWhatsapp />
-            Whatsapp
-          </Link>
+          <div className="flex gap-2 select-none">
+            <Link
+              href={`https://wa.me/${process.env.NEXT_PUBLIC_WHATSAPP_NUMBER}`}
+              target="_blank"
+              className="p-2 rounded-sm flex items-center gap-1 hover:underline underline-offset-2 select-none hover:bg-gradient-to-r from-[#128c7e]/60 to-[#25d366]/60"
+            >
+              <FaWhatsapp />
+              Whatsapp
+            </Link>
+            <Link
+              href={`https://www.instagram.com/${process.env.NEXT_PUBLIC_INSTAGRAM_USERNAME}`}
+              target="_blank"
+              className="p-2 rounded-sm flex items-center gap-1 hover:underline underline-offset-2 hover:bg-gradient-to-r from-[#5b51d8]/60 via-[#c13584]/60 to-[#f77737]/60"
+            >
+              <FaInstagram />
+              Instagram
+            </Link>
+          </div>
         </div>
         <p className="pt-4 text-muted-foreground">
           &copy; Criado por{" "}
